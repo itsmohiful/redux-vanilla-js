4310580132e105746baf617409a8a3c7d16e3021
itsmohiful <mohiful.islam.ofc@gmail.com>
2024-06-04 19:10:57 +0600

    add match, delete, reset, increase, decrease functionality added

--- js/redux.js
+++ js/redux.js
@@ -1,6 +1,6 @@
 //initial state
 const initialState = {
-  matches: [{ id: 1, score: 0 }],
+  matches: [{ id: 1, score: 100 }],
 };
 
 // Action types
@@ -9,18 +9,19 @@ const DELETE_MATCH = "DELETE_MATCH";
 const INCREMENT_SCORE = "INCREMENT_SCORE";
 const DECREMENT_SCORE = "DECREMENT_SCORE";
 const RESET_SCORES = "RESET_SCORES";
+const DELETE_ALL = "DELETE_ALL";
 
 // action creators
-const addMatch = () => ({ type: ADD_MATCH });
+export const addMatch = () => ({ type: ADD_MATCH });
 
-const deleteMatch = (id) => ({
-  type: ADD_MATCH,
+export const deleteMatch = (id) => ({
+  type: DELETE_MATCH,
   payload: {
     id: id,
   },
 });
 
-const incrementScore = (id, score) => ({
+export const incrementScore = (id, score) => ({
   type: INCREMENT_SCORE,
   payload: {
     id: id,
@@ -28,23 +29,25 @@ const incrementScore = (id, score) => ({
   },
 });
 
-const decrementScore = (id, score) => ({
-  type: INCREMENT_SCORE,
+export const decrementScore = (id, score) => ({
+  type: DECREMENT_SCORE,
   payload: {
     id: id,
     score: score,
   },
 });
 
-const resetScore = () => ({ type: RESET_SCORES });
+export const resetScore = () => ({ type: RESET_SCORES });
+
+export const deleteAll = () => ({ type: DELETE_ALL });
 
 //create reducer function
-function createReducer(state = initialState, action) {
+export function createReducer(state = initialState, action) {
   switch (action.type) {
     case ADD_MATCH:
       return {
         ...state,
-        matches: [...state?.matches, { id: Date.now(), score: 0 }],
+        matches: [...state?.matches, { id: Date.now(), score: 100 }],
       };
 
     case DELETE_MATCH:
@@ -60,7 +63,7 @@ function createReducer(state = initialState, action) {
         ...state,
         matches: state?.matches?.map((match) =>
           match?.id === action?.payload?.id
-            ? { ...match, score: match?.score + action?.payload?.value }
+            ? { ...match, score: match?.score + action?.payload?.score }
             : match
         ),
       };
@@ -70,7 +73,13 @@ function createReducer(state = initialState, action) {
         ...state,
         matches: state?.matches?.map((match) =>
           match?.id === action?.payload?.id
-            ? { ...match, score: match?.score - action?.payload?.value }
+            ? {
+                ...match,
+                score:
+                  match?.score > action?.payload?.score
+                    ? match?.score - action?.payload?.score
+                    : 0,
+              }
             : match
         ),
       };
@@ -81,19 +90,10 @@ function createReducer(state = initialState, action) {
         matches: state?.matches?.map((match) => ({ ...match, score: 0 })),
       };
 
+    case DELETE_ALL:
+      return initialState;
+
     default:
       return state;
   }
 }
-
-//create store
-const store = Redux.creatStore(createReducer);
-
-//render method
-const render = () => {
-  const state = store?.getStates();
-  return;
-};
-
-//update ui initally
-render();
--- js/script.js
+++ js/script.js
@@ -1,1 +1,123 @@
+import {
+  addMatch,
+  createReducer,
+  decrementScore,
+  deleteAll,
+  deleteMatch,
+  incrementScore,
+  resetScore,
+} from "./redux.js";
 
+//create store
+const store = Redux.createStore(createReducer);
+
+//render methods
+const renderMathces = () => {
+  const state = store.getState();
+
+  const matchesContainer = document.querySelector(".all-matches");
+  matchesContainer.innerHTML = "";
+
+  state?.matches?.map((match, index) => {
+    const matchElement = document.createElement("div");
+    matchElement.classList.add("match", "pb-4");
+
+    matchElement.innerHTML = `
+        <div class="wrapper">
+            
+            <button class="delete" data-id="${match.id}">
+                <img src="./images/delete.svg" alt="" />
+            </button>
+            <h3 class="matchName">Match ${index + 1}</h3>
+        </div>
+
+        <div class="inc-dec">
+            <form class="incrementForm">
+                <h4>Increment</h4>
+                <input type="number" name="increment" class="increment" data-id="${
+                  match?.id
+                }"/>
+            </form>
+
+            <form class="decrementForm">
+                <h4>Decrement</h4>
+                <input type="number" name="decrement" class="decrement" data-id="${
+                  match?.id
+                }"/>
+            </form>
+        </div>
+
+          <div class="numbers">
+            <h2 class="singleResult">${match?.score}</h2>
+          </div>
+    `;
+
+    //add event listener in delete button
+    matchElement.querySelector(".delete").addEventListener("click", () => {
+      deleteMatchFunc(match?.id);
+    });
+
+    //add event listener for increment score
+    matchElement
+      .querySelector(".incrementForm")
+      .addEventListener("submit", (event) => {
+        incrementScoreFunc(event, match?.id);
+      });
+
+    //add event listener for decrement score
+    matchElement
+      .querySelector(".decrementForm")
+      .addEventListener("submit", (event) => {
+        decrementScoreFunc(event, match?.id);
+      });
+
+    //match div append
+    matchesContainer.appendChild(matchElement);
+  });
+};
+
+//subscribe store
+store.subscribe(renderMathces);
+
+//initially render
+renderMathces();
+
+//add matches
+document.querySelector(".addMatch").addEventListener("click", () => {
+  store.dispatch(addMatch());
+});
+
+//delete match
+const deleteMatchFunc = (matchId) => {
+  store.dispatch(deleteMatch(matchId));
+};
+
+//increment match's score
+const incrementScoreFunc = (event, matchId) => {
+  event.preventDefault();
+  const score = parseInt(event.target[0].value);
+  console.log(score, matchId);
+
+  store.dispatch(incrementScore(matchId, score));
+};
+
+//decrement match's score
+const decrementScoreFunc = (event, matchId) => {
+  event.preventDefault();
+  const score = parseInt(event.target[0].value);
+  console.log(score, matchId);
+
+  store.dispatch(decrementScore(matchId, score));
+};
+
+//reset scores
+document.querySelector(".reset").addEventListener("click", () => {
+  store.dispatch(resetScore());
+});
+
+//delete all match's
+document.querySelector(".btn-clear").addEventListener("click", () => {
+  //   console.log(window.location);
+  //   window.location.replace("http://127.0.0.1:5500/index.html");
+  store.dispatch(deleteAll());
+});
